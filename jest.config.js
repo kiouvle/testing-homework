1b1c7134faa85b9d84ca9dc03b2a0a774b28bd95
kiouvle <zhenyamaltseva@ya.ru>
2023-07-02 16:53:55 +0300

    Updates jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  // https://stackoverflow.com/questions/56547215/react-testing-library-why-is-tobeinthedocument-not-a-function
+  setupFilesAfterEnv: ["<rootDir>/test/unit/setupTests.js"]
 };
